032c6c9bff1a109606f2026bd5eb6a18c239aed3
Hiren <rebeldevmurari@gmail.com>
2022-12-04 14:37:04 +0000

    Initial commit

---
+++ src/main/java/com/rebel/DogGrapgQLApi/DogGrapgQlApiApplication.java
@@ -0,0 +1,13 @@
+package com.rebel.DogGrapgQLApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogGrapgQlApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogGrapgQlApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebel/DogGrapgQLApi/DogGrapgQlApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebel.DogGrapgQLApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogGrapgQlApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
